95cea7dba943448765cd1f06f64469433f5d0f2f
Kyle Whelan <kyle.aj.whelan@outlook.com>
2022-10-02 23:22:40 +0100

    search function added

---
+++ app.js
@@ -0,0 +1,33 @@
+const searchBtn = document.querySelector('.search-button').addEventListener('click', e => {
+    const user = document.getElementById('search').value;
+    console.log(user);
+    fetchUser(user);
+
+    e.preventDefault();
+});
+const userInfoContainer = document.querySelector('.user-info');
+
+
+function fetchUser(username) {
+    fetch(`https://api.github.com/users/${username}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            // console.log(response.avatar_url);
+            displayData(response.avatar_url, response.login, response.followers, response.public_repos, response.url)
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+function displayData(avatar, username, followers, repoCount, url) {
+    userInfoContainer.innerHTML = `
+    <h2><a href=${url}>${username}</a></h2>
+    <img src="${avatar} class="avatar" width="128px" height="128px">
+    <h4>${followers} followers<h4> <h4>${repoCount} public repos</h4>`;
+
+}
+
+
